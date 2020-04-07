a7f0568f1a495ce4c6b7adb05a1537ceec7a5d25
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-04-07 16:56:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,7 +25,7 @@ class CashRegister
  
   def void_last_transaction
     @total -= self.last_transaction = @total
-    self.last_transaction -= @total.last  = @total 
+    self.last_transaction -= @total/100 = @total 
   end 
 
 
